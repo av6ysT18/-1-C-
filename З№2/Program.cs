557a0ef92e456d582010f4b47fbd9659426bb960
mikhail <mipugacheff@yandex.ru>
2022-11-20 02:42:25 +0300

    №2

---
+++ З№2/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numA = 10;
+        int numB = 17;
+
+        if (numA > numB)
+        {
+            Console.WriteLine($"{numA} > {numB}");
+
+        }
+        else{
+            Console.WriteLine($"{numA} < {numB}");
+        }
+    }
+}
