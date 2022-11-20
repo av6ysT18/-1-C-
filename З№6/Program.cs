58433bfa935dfd8b1acd52dc8ecbd0769334d9f7
mikhail <mipugacheff@yandex.ru>
2022-11-20 05:04:12 +0300

    #6

---
+++ З№6/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int number; 
+         
+        Console.WriteLine("Ведите число");
+        number = Convert.ToInt32(Console.ReadLine());
+
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("Число четное");
+        }
+        else
+        {
+            Console.WriteLine("Число не четное");
+        }
+    }
+}
