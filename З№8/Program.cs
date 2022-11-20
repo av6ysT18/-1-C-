793da0a404a72e1efc389621e8ecda3a49961169
mikhail <mipugacheff@yandex.ru>
2022-11-20 19:53:27 +0300

    #8

---
+++ З№8/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int number;
+
+        Console.WriteLine("Ведите число");
+        number = Convert.ToInt32(Console.ReadLine());
+
+        int count = 1;
+        while (count <= number)
+        {
+           if(count % 2 == 0 ) Console.Write(count);
+            count++;
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
