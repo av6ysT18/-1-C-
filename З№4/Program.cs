c5dd3998987d5c567acc46d21b3843cfdac06a74
mikhail <mipugacheff@yandex.ru>
2022-11-20 04:25:16 +0300

    #4

---
+++ З№4/Program.cs
@@ -0,0 +1,37 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numA = 14;
+        int numB = 8;
+        int numC = 19;
+        int max = numA;
+        if (numB >max) max = numB;
+        if (numC >max) max = numC;
+        Console.Write(max);
+        // решение чуток подсмотрел у вас, просто не знал как синтаксически вывести
+    
+       
+      
+
+
+
+         
+        
+         
+            
+    }
+          
+}
+
+    
+
+     
+        
+   
+        
+        
+
+        
+    
