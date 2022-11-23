67edc98850a742091523ff2d4c7efa7076ccc8fc
mikhail <mipugacheff@yandex.ru>
2022-11-23 23:23:27 +0300

    З#15

---
+++ З№15/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Ведите число от 1 до 7 обозначающие дни недели");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 6 | number == 7)
+{
+ Console.WriteLine("Ура, выходной!");
+}
+else
+{
+  Console.WriteLine("Все еще рабочая неделя");  
+}
