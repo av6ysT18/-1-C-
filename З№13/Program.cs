843ded22b0185ba62c2ba4e246f5edc5fe9f714e
mikhail <mipugacheff@yandex.ru>
2022-11-23 23:22:44 +0300

    З#13

---
+++ З№13/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+int number = 1233647623;
+Console.WriteLine(Convert.ToString(number)[2]); // думал о решении с массивами, но пришлось посмотреть как)
+*/
+int number =123457976;
+
+while(number > 1000)
+{
+    number/=10;
+    Console.WriteLine(number); //
+}
+Console.WriteLine("третья цифра" + number%10);
